5ce76c7a2ab2f97fea4ec036b9ff448a9c912cb7
Georgii_Bystrov <georgii_bystrov@epam.com>
2021-04-27 13:55:51 +0300

    Init commit

---
+++ src/test/java/registrationService/reg/RegApplicationTests.java
@@ -0,0 +1,13 @@
+package registrationService.reg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
